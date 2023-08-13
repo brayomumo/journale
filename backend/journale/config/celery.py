ee5a6ad1928c56af265dc69b4527ab800900046d
brian.mumo <brayomumo5@gmail.com>
2023-08-13 10:42:38 +0300

    Add Periodic task to send journal daily at 12pm

--- backend/journale/config/celery.py
+++ backend/journale/config/celery.py
@@ -6,3 +6,14 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'journale.config.settings')
 app = Celery('journale')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
+from celery.schedules import crontab
+from datetime import datetime
+
+CELERY_BEAT_SCHEDULE = { 
+    'Task_one_schedule' : { 
+        'task': 'journale.journal.task.send_journals', 
+        'schedule': crontab(hour=12), # Run every 12pm
+    }
+}
--- backend/journale/journal/task.py
+++ backend/journale/journal/task.py
@@ -1,8 +1,30 @@
-from journale.journal.models import Journal
+import random
+from django.core.mail import send_mail
+from django.conf import settings
 
+from journale.journal.models import Journal
+from journale.wenyeji.models import User
+
+
+
+def get_random_journal(journals, user_id):
+    user = User.objects.get(id=user_id)
+    index = random.randint(0, journals.count())
+    journ = journals[index]
+    if journ.owner == user:
+        return get_random_journal(journals, user_id)
+    else:
+        return user, journ
 
 
 def send_journals():
     journals = Journal.last_24_hrs()
-    users = journals.values_list("owner")
-    print(journals.count(), users.count())
+    users = set(journals.values_list("owner", flat=True))
+    for user in users:
+        u, journal = get_random_journal(journals, user)
+        send_mail(
+            "Daily Random Journal",
+            journal.text,
+            settings.EMAIL_HOST_USER,
+            [u.email]
+        )
